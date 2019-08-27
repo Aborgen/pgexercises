ba848a8450b2897aedc4645693b97b381dd78d60
Alexander Borgen <alexanderborgen@gmail.com>
2019-08-27 14:38:32 -0700

    Fix casing

--- Aggregation.sql
+++ Aggregation.sql
@@ -522,7 +522,7 @@ WHERE totalSlots = (
 Intended solution:
 SELECT facid, total
 FROM (
-  SELECT facid, SUM(slots) AS total, RANK() OVER (ORDER by SUM(slots) DESC) AS rank
+  SELECT facid, SUM(slots) AS total, RANK() OVER (ORDER BY SUM(slots) DESC) AS rank
   FROM cd.bookings
   GROUP BY facid
 ) AS ranked
